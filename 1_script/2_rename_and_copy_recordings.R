78a88edac7067b50e63495f8c3d363930bee65ab
Jeremiah Kennedy <jeremiah.kennedy@hemmera.com>
2021-11-21 09:29:56 -0800

    writing and updating copying, renaming script
    Writing and updating Combibning resulting index file script for grid of index files

--- 1_script/2_rename_and_copy_recordings.R
+++ 1_script/2_rename_and_copy_recordings.R
@@ -11,23 +11,26 @@
 # Re-set  your script when needed
 dev.off()
 rm(list=ls())
-
+library(stringr)
 
 # Specify directory where files are kept:
-orig_dir = 
-  "//hemmera.com/Shared/ProjectScratch/106242-01 Bird and Bat Data/Rayrock Bird Data - RAW/
-    Check 1/SB02/Data" # where files are kept and not modified
 
-cop_dir = "C:/Users/jeremiah.kennedy/Documents/Rayrock/SB02/Data" # where files are copied to and modified there
+
+
+orig_dir = "//hemmera.com/Shared/ProjectScratch/106242-01 Bird and Bat Data/Rayrock Bird Data - RAW/Check 2/SB02" # where files are kept and not modified
+
+cop_dir = "C:/Users/jeremiah.kennedy/Documents/Rayrock/Check 2/SB02/Data" # where files are copied to and modified there
 
 # create directory 
 dir.create(cop_dir,recursive = T)
 
 # Copy files
-file.copy(list.files(orig_dir,pattern = "*.wav"),cop_dir)
+files<-list.files(orig_dir,pattern = "*.wav",recursive = T,full.names = T) # list
+file.copy(files,cop_dir,recursive = T) # copy
 
 # Set directory to where your recordings are
 setwd(cop_dir)
 
 file.rename(list.files(pattern = "*.wav"), str_replace(list.files(pattern = "*.wav"),pattern = ".wav",replacement = "-0700.wav"))
-library(stringr)
+
+
---
+++ 1_script/3_Combine_Index_Visualizations.R
@@ -0,0 +1,50 @@
+#################################################################
+########### Combine LDFCS for rapid assessment ##################
+#################################################################
+
+
+# Re-set  your script when needed
+dev.off()
+rm(list=ls())
+
+# packages
+library(OpenImageR)
+
+## Required settings ##
+dir_sep = "C:/Users/jeremiah.kennedy/Documents/Working/Rayrock Output/RB03/index_nights" # where the files are kept
+dir_comb = "C:/Users/jeremiah.kennedy/Documents/Working/Rayrock Output/Combined/RB03" # where the combined files are to go
+station = "RB03" # set station where recordings are from
+nights = 8 # number of nights/days to combine
+cols = 4 # number of columns to organize results into
+
+
+# create directory for output
+dir.create(dir_comb,recursive = T)
+
+# list in files to make into jpg
+img<-list.files(path = dir_sep,pattern = "*2Maps.png",recursive = T,full.names = T)
+
+
+# create the name to be given to the file being written
+out.name=file.path(dir_comb,paste0(station,"_indices_grid",".jpg"))
+
+jpeg(filename = out.name, width = 3000, height = 2000, res = 1200)
+par(mai=rep(0,4)) # no margins required 
+# You can change 25 to length img, but if it's not divisable by ncol then you're pooched
+layout(matrix(1:nights, ncol=cols, byrow=TRUE)) # set layout of your plots depending on howmany recordings etc
+
+
+# loop for filling in layout
+# i=1
+for(i in 1:length(img)) { # complete for all images in list-img
+  # subset to get night ID (ynight)
+  id<-basename(dirname(img[i])) # personalize depending on name
+  plot(NA,xlim=0:1,ylim=0:1, bty="n", xaxt='n', yaxt='n') # get rid of xy values and marks
+  rasterImage(readImage(img[i]),0,0,1,1) # print image
+  text(x=0.07,y=0.07,labels = id,col="white",cex=0.3)
+}
+
+dev.off()
+
+
+
