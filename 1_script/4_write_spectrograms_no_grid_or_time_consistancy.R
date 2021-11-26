b90b1baea67b47e90b196c9bc96d4e2238a4be5c
Jeremaih Kennedy <jeremiah.kennedy@hemmera.com>
2021-11-26 10:29:09 -0800

    Modified loop to make station nights manipulable. ie itterate through first 4 then next 4 then... and so on. THis just means you can run the script over clusters of nights and keep the visualization on the same 4 frame image

--- 1_script/4_write_spectrograms_no_grid_or_time_consistancy.R
+++ 1_script/4_write_spectrograms_no_grid_or_time_consistancy.R
@@ -17,6 +17,8 @@ library(lubridate)
 library(sound)
 library(tuneR)
 library(oce)
+library(av)
+
 
 # Combination of:
 # 1) append_dates&select....R
@@ -30,8 +32,13 @@ library(oce)
 
 
 # Set directories 
-in.root = "S:/ProjectScratch/398-173.07/ARUs - 2021/rw/210621_SE05" # recording files location
+in.root = "S:/ProjectScratch/398-173.07/ARUs - 2021/rw/" # recording files location
 out.root="C:/Users/jeremiah.kennedy/Documents/Working/SiteC_output/Spectrograms/" # output directory where results are kept
+
+## Additional input directory options
+# multiple (but not all) sub-directories 
+subs = c("210621_SE05","210716_2536_SE05","SM4") # list subdirectories
+in.roots = paste0(in.root,subs) # paste to make full directories 
 
 # set required numbger of grid cells for reference
 ncells = 4
@@ -44,19 +51,10 @@ spec.max=10 # spectrogram max frequency in khz
 spec.min=0 # spectrogram min frequency in khz
 transf = 2048 # Forier transformation to use (wl in spectro function)
 
-# Let's check what we know about these recordings (SKIP IF YOU ALREADY KNOW)
-# 
-
-
-# the following can be set specifically in the loops if need be (see other write spectrograms scrpt)
-Length=3600 # total length of recording in seconds
-Breaks=seq(0,Length,Interval) # sequence of break locations 
-
 
 # Get files
-full.file = list.files(in.root,recursive = T,full.names = T,pattern = "*.wav") # full file names
+full.file = list.files(in.roots,recursive = T,full.names = T,pattern = "*.wav") # full file names
 file.name = basename(full.file)
-
 
 
 # Metadata  and sorting
@@ -79,6 +77,19 @@ meta$size = as.numeric(file.size(meta$file.name))
 good = meta[yday(meta$Date)==meta$or.day,]
 
 
+# Let's check what we know about these recordings (SKIP IF YOU ALREADY KNOW)
+# How long are the recordings 
+
+av_media_info(meta$file.name[1])
+
+# the following can be set specifically in the loops if need be (see other write spectrograms scrpt)
+Length=1800 # total length of recording in seconds
+Breaks=seq(0,Length,Interval) # sequence of break locations 
+
+
+
+
+
 
 # some QAQC
 # missing.dat = meta[meta$size == 0,]
@@ -91,98 +102,70 @@ meta = meta[meta$size>0,]
 meta_2 = meta %>% group_by(station_date) %>% mutate(night.seq = order(time)) %>% arrange(station_date,night.seq) 
 
 
-## Before continuing.... ###
-
-
-#### Make csv for tracking visualization of acoustic indices
-
-# columns Required 
-# - prefix, date, station_date, recording count
-
-ind_track = meta %>% group_by(prefix, Date, station_date) %>% summarise(recs = n())
-
-
-# We will set the Threshold of min recs per day for processing to be 10 (representing 10 hours)
-ind_track = ind_track[ind_track$recs>9,]
-ind_track$date = as.Date(ind_track$Date)
-
-# Save as CSV for processing
-# write.csv(ind_track,file = "C:/Users/jeremiah.kennedy/Documents/Rayrock/Tracking/Process_Spec_Vis.csv",row.names = F)
-
-# now, bring that back and use it to filter recordings further
-ind_track = read.csv(file = "C:/Users/jeremiah.kennedy/Documents/Rayrock/Tracking/Process_Spec_Vis.csv")
-# You can check howmany you've chozen and decide if you want to filter further 
-# stns = ind_track[ind_track$Use=="y",] %>% group_by(prefix) %>% summarise(count = n())
-
-chosen = ind_track[ind_track$Use=="y",]
-
-## Filter by station nights chosen
-
-meta_3 = meta_2[meta_2$station_date %in% chosen$station_date,]
-
 ## Make a file to process data in 
 # Get basename
-meta_3$base.name = basename(meta_3$file.name)
-proc_file = meta_3[c("base.name","Date","or.day","night.seq","prefix","time")]
+meta_2$base.name = basename(meta_2$file.name)
+proc_file = meta_2[c("base.name","Date","or.day","night.seq","prefix","time")]
 
 dat_pics = data.frame(matrix(NA,nrow = 0,ncol = 1))
 colnames(dat_pics) = "pic_name"
 
-meta_3 = meta_3[meta_3$prefix=="RAYBIRD3",]
 
-# 
-# # naming loop
-# for (site in unique(meta_3$prefix)){
-#   dir.out = paste0(out.root,site,"/")
-#   
-#   # Keep only appropriate site data
-#   dat_in = meta_3[meta_3$prefix==site,]
-#   
-#   for (i in 1:max(dat_in$night.seq)){
-#     
-#     dat_mid = dat_in[dat_in$night.seq==i,]
-#     
-#     dat_mid = dat_mid[order(dat_mid$or.day,decreasing = F),]
-#   
-#     
-#     # loop through 60 sec periods
-#     #k=2
-#     for (k in 1:(length(Breaks)-1)){
-#       
-#       Start = Breaks[k]
-#       End = Breaks[k+1]
-#       
-#       
-#       # create name for each time image
-#       
-#       name=paste0(site,"_",formatC(dat_mid$night.seq[1],width = 2,flag = 0),"_",formatC(Start, width = 3,flag = 0))
-#       name=paste(name,"jpeg",sep = ".")
-#       
-#       
-#       if (site == unique(meta_3$prefix)[1] & i==1 & k==1){
-#         
-#         dat_pics <-c(name)
-#         
-#       } else (dat_pics <- c(dat_pics,name))
-#       
-#     
-#       
-#       
-#     } 
-#     
-#   }
-# }
+# naming loop
+for (site in unique(meta_2$prefix)){
+  dir.out = paste0(out.root,site,"/")
+
+  # Keep only appropriate site data
+  dat_in = meta_2[meta_2$prefix==site,]
+
+  for (i in 1:max(dat_in$night.seq)){
+
+    dat_mid = dat_in[dat_in$night.seq==i,]
+
+    dat_mid = dat_mid[order(dat_mid$or.day,decreasing = F),]
+
+
+    # loop through 60 sec periods
+    #k=2
+    for (k in 1:(length(Breaks)-1)){
+
+      Start = Breaks[k]
+      End = Breaks[k+1]
+
+
+      # create name for each time image
+
+      name=paste0(site,"_",formatC(dat_mid$night.seq[1],width = 2,flag = 0),"_",formatC(Start, width = 3,flag = 0))
+      name=paste(name,"jpeg",sep = ".")
+
+
+      if (site == unique(meta_2$prefix)[1] & i==1 & k==1){
+
+        dat_pics <-c(name)
+
+      } else (dat_pics <- c(dat_pics,name))
+
+
+
+
+    }
+
+  }
+}
 
 
 # save
-# write.csv(dat_pics,file = "C:/Users/jeremiah.kennedy/Documents/Working/Rayrock Output/Spectrograms/tracking.csv")
+
+# dir.create("C:/Users/jeremiah.kennedy/Documents/Working/SiteC_output/Spectrograms/",recursive = T)
+# write.csv(dat_pics,file = "C:/Users/jeremiah.kennedy/Documents/Working/SiteC_output/Spectrograms/site_C_JPGs.csv")
+
 
 # now we are ready for forming spectrograms
 # meta_3 = meta_3[substr(meta_3$prefix,1,7)=="SUNBIRD",]
 
 ###### Part 2 ~ Spectrogram Formation ####### 
-# site = unique(meta_3$prefix)[1]
-for (site in unique(meta_3$prefix)){
+# site = unique(meta_2$prefix)[1]
+for (site in unique(meta_2$prefix)){
   
   
   # Create directory for site specs
@@ -193,7 +176,7 @@ for (site in unique(meta_3$prefix)){
   
   
   # Keep only appropriate site data
-  dat_in = meta_3[meta_3$prefix==site,]
+  dat_in = meta_2[meta_2$prefix==site,]
   
   # Loop through sessions 
   #i=1
@@ -203,31 +186,41 @@ for (site in unique(meta_3$prefix)){
     
     dat_mid = dat_mid[order(dat_mid$or.day,decreasing = F),]
     
+    all_nights = unique(dat_mid$or.day)
     
-    
-    # loop through 60 sec periods
-    #k=1
-    for (k in 1:(length(Breaks)-1)){
+   # j = 2
+    for (j in 1:floor((length(unique(dat_mid$or.day)))/ncells)){
       
-      Start = Breaks[k]
-      End = Breaks[k+1]
-      
-      
-      # create name for each time image
-    
-      name=paste0(site,"_",formatC(dat_mid$night.seq[1],width = 2,flag = 0),"_",formatC(Start, width = 3,flag = 0))
-      name=paste(name,"jpeg",sep = ".")
+      start_night = (j-1)*4+1
+      end_night = start_night+3
+      grp_night = all_nights[start_night:end_night]
       
       
-    
-      # create jpeg with "name" and specify size
-      jpeg(paste0(dir.out,name), width=13, height=8, units='in', res=800)
-      par(mfrow=c(height,width)) 
-      par(mar=rep(0,4))
+      dat_mid = dat_mid[dat_mid$or.day %in% grp_night,]
       
+      # loop through 60 sec periods
+      #k=1
+      for (k in 1:(length(Breaks)-1)){
         
-      
-      # draw JPG Looping through 4 dates
+        Start = Breaks[k]
+        End = Breaks[k+1]
+        
+        
+        # create name for each time image
+        
+        name=paste0(site,"_",formatC(dat_mid$night.seq[1],width = 2,flag = 0),"_",formatC(Start, width = 3,flag = 0))
+        name=paste(name,"jpeg",sep = ".")
+        
+        
+        
+        # create jpeg with "name" and specify size
+        jpeg(paste0(dir.out,name), width=13, height=8, units='in', res=800)
+        par(mfrow=c(height,width)) 
+        par(mar=rep(0,4))
+        
+        
+        
+        # draw JPG Looping through 4 dates
         # L=1
         for(L in 1:ncells) {
           section = dat_mid$file.name[L]
@@ -245,13 +238,19 @@ for (site in unique(meta_3$prefix)){
         }
         dev.off()
       }
-        
-        
-        
-    # Track sessions
-    print(paste0("Session ",i," of ", max(dat_in$night.seq)," site ",site))    
-  }
-  
+      
+      
+      
+      # Track sessions
+      print(paste0("Session ",i," of ", max(dat_in$night.seq)," site ",site))    
+    }
+    
+      
+      
+      
+      
+    }
+    
   
   cat("\n")
   print(paste0(site,"-COMPLETE"))
