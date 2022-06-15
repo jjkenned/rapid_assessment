bd860ab5ffbdc7a49f5ef005cf92ffe08229bd52
Jeremiah Kennedy <34352550+jjkenned@users.noreply.github.com>
2022-06-14 18:22:32 -0700

    Update 2_Acoustic_Indices_By_Station.ps1

--- 1_script/2_Acoustic_Indices_By_Station.ps1
+++ 1_script/2_Acoustic_Indices_By_Station.ps1
@@ -1,39 +1,11 @@
-﻿<#
-.SYNOPSIS
-  Generates acoustic indices for multiple audio files and concatenates the output.
-.DESCRIPTION
-  Generates acoustic indices for multiple audio files and concatenates the output.
-  Expects each directory to contain audio files for one location.
-.PARAMETER $input_directories
-    A directory of audio files to process.
-.PARAMETER $output_directory
-    A directory where the indices should be exported to
-.INPUTS
-  Optionally: input directories
-.OUTPUTS
-  Files stored in $output_directory
-.NOTES
-  Version:        2.0
-  Author:         Anthony Truskinger
-  Creation Date:  2020-01-30
-  Modification Date: 2021-11-18
-  Purpose/Change: Updated docs links, add ap finder script
-.EXAMPLE
-  ./indices_and_concat.ps1 D:/Stud D://Thompson -time_zone_offset "10:00" -output_directory ./output
-#>
-
-
-# Parameters 
-# Directories and naming
-# The character string used to define the directories may not get recognized as directories so I found a workaround
 
 # Set-Location -Path "E:\processing\copied_recordings\BIRD\2022\MKVI"
-$group = "MKVI-U06" # station to station basis at this point
+$group = "MKVI-U-14" # station to station basis at this point
 
 # set in and out directories 
-$parent_input_dir = "E:\processing\copied_recordings\BIRD\2022\MKVI" 
+$parent_input_dir = "P:/PMRA_processing/copied_recordings/BIRD/2022/MKVI" 
 $input_directories = Get-Childitem -Path "$parent_input_dir/$group"
-$output_directory = "E:\processing\output.index.values\BIRD\2022\MKVI" # output directory 
+$output_directory = "P:/PMRA_processing/output.index.values/BIRD/2022/MKVI" # output directory 
 $name_filter = "*" # name filter(kinda unsure what it means)
 $time_zone_offset = -0700
 
@@ -44,7 +16,7 @@ $ErrorActionPreference = "Stop"
 
 
 # get the path for AP.exe. When do this to resolve some nice default config files.
-# TODO: remove this when the default config file feature is implemented in AP.exe
+
 $ap_path = "C:\AP"
 $default_configs = Resolve-Path "$ap_path\ConfigFiles"
 
@@ -76,7 +48,7 @@ foreach ($input_directory in $input_directories) {
         
         # for more information on how this command works, please see:
         # https://ap.qut.ecoacoustics.info/technical/commands/analyze_long_recording.html
-        C:\AP\AnalysisPrograms.exe audio2csv $file "$default_configs/Towsey.Acoustic.yml" "$output_directory/by_rec/$group/$current_group/$name" --no-debug --parallel 
+        C:\AP\AnalysisPrograms.exe audio2csv $file "$default_configs/Towsey.Acoustic.Short.Low.yml" "$output_directory/by_rec/$group/$current_group/$name" --no-debug --parallel 
 
     
     
@@ -88,7 +60,7 @@ foreach ($input_directory in $input_directories) {
     # https://ap.qut.ecoacoustics.info/technical/commands/concatenate_index_files.html
     C:\AP\AnalysisPrograms.exe ConcatenateIndexFiles `
         --input-data-directory "$output_directory/by_rec/$group/$current_group" `
-        --output-directory "$output_directory/by_night" `
+        --output-directory "$output_directory/by_night/$group" `
         -z $time_zone_offset `
         --file-stem-name $current_group `
         --directory-filter "*.*" `
@@ -100,6 +72,7 @@ foreach ($input_directory in $input_directories) {
 }
 
 Write-Output "Complete!"
+
 
 
 
